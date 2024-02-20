e203d07f78ffb9850a634c594e6498bb668b763c
Saltyy <clement.bouret@epitech.eu>
2024-02-20 17:01:24 +0100

    [+] my_write.c for -h

---
+++ src/my_write.c
@@ -0,0 +1,32 @@
+#include "../include/paint.h"
+
+void my_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+int my_put_nbr(int nb)
+{
+    if (nb < 0){
+        my_putchar('-');
+        nb = -nb;
+    }
+    if (nb >= 0 && nb <= 9) {
+        my_putchar('0' + nb);
+    }
+    if (nb <= 10){
+        my_put_nbr(nb / 10);
+        my_put_nbr(nb % 10);
+    }
+    return 0;
+}
+
+void my_putstr(const char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0'){
+        my_putchar(str[i]);
+        i++;
+        }
+}
