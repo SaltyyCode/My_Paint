78eedf4be91b8916d203070fd96f51ff322fd566
Saltyy <clement.bouret@epitech.eu>
2024-02-20 17:01:44 +0100

    [~] addition of -h handling

--- src/main.c
+++ src/main.c
@@ -4,6 +4,11 @@ int main(int ac, char **av)
 {
     Window *app = malloc(sizeof(Window));
 
+    if(ac == 2 && av[1][0] == '-' && av[1][1] == 'h'){
+        get_help();
+        return 0;
+    }
+
     test_paint(app);
     free(app);
     return 0;
