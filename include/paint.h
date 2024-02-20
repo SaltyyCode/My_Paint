cb0df7af1e8998a49b4c4f6e9a70a23dc73c0d71
Saltyy <clement.bouret@epitech.eu>
2024-02-20 15:34:38 +0100

    [+].h for paint

---
+++ include/paint.h
@@ -0,0 +1,32 @@
+#ifndef PAINT
+    #define PAINT
+
+    #include <SFML/Graphics.h>
+    #include <SFML/Graphics/RenderWindow.h>
+    #include <SFML/Graphics/Color.h>
+    #include <SFML/Graphics/Rect.h>
+    #include <SFML/Audio.h>
+    #include <SFML/System.h>
+    #include <SFML/System/Clock.h>
+    #include <SFML/Window.h>
+    #include <stdio.h>
+    #include <unistd.h>
+    #include <stdlib.h>
+    #include <time.h>
+
+typedef struct window {
+    sfRenderWindow *window;
+    sfVideoMode video_mode;
+    sfEvent event;
+    sfTexture *bgtexture;
+    sfSprite *bgsprite;
+} Window;
+
+//my_event.c
+int analyse_event(Window *app);
+
+//my_window.c
+void draw_window(Window *app);
+void del_window(Window *app);
+
+#endif /*PAINT*/
