9d9d098ffe0557028bbfdd79d6fd291d09c5b652
Saltyy <clement.bouret@epitech.eu>
2024-02-20 17:01:11 +0100

    [+] my_help.c for -h

---
+++ src/my_help.c
@@ -0,0 +1,10 @@
+#include "../include/paint.h"
+
+void get_help()
+{
+    my_putstr("My_Paint epitech project !\n\n");
+    my_putstr("Tools:\n");
+    my_putstr("Pencil : Press P and click or drag to draw\n");
+    my_putstr("Eraser: Press E and click or drag to erase\n");
+    my_putstr("Hand: Press H and click or drag to move canva\n");
+}
