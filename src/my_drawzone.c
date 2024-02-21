53d2a4dc2bd9c4f3c0d2833b788eec28cc470189
Saltyy <clement.bouret@epitech.eu>
2024-02-21 22:55:15 +0100

    [+] functions to create and display the drawzone

---
+++ src/my_drawzone.c
@@ -0,0 +1,21 @@
+#include "../include/paint.h"
+
+void init_drawzone(Window *app)
+{
+   const int reserved = 100;
+
+   app->drawzone = sfRenderTexture_create(app->video_mode.width, app->video_mode.height - reserved, false);
+   sfRenderTexture_clear(app->drawzone, sfWhite);
+   sfRenderTexture_display(app->drawzone);
+   
+}
+
+void display_drawzone(Window *app)
+{
+    sfTexture* texture = sfRenderTexture_getTexture(app->drawzone);
+    sfSprite* sprite = sfSprite_create();
+    sfSprite_setTexture(sprite, texture, sfTrue);
+    sfSprite_setPosition(sprite, (sfVector2f){0, 100});
+    sfRenderWindow_drawSprite(app->window, sprite, NULL);
+    sfSprite_destroy(sprite);
+}
