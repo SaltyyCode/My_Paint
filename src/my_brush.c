d7c7a4046a25127db85738f3afee34417a618c52
Saltyy <clement.bouret@epitech.eu>
2024-02-22 00:05:35 +0100

    [+] draw with a brush functions, still not finished only draw points

---
+++ src/my_brush.c
@@ -0,0 +1,18 @@
+#include "../include/paint.h"
+
+void initbrush(Brush *brush, sfColor color, float radius)
+{
+    brush->color = color;
+    brush->radius = radius;
+}
+
+void drawbrush(sfRenderTexture* renderTexture, Brush *brush, sfVector2f pos)
+{
+    sfCircleShape* shape = sfCircleShape_create();
+    sfCircleShape_setRadius(shape, brush->radius);
+    sfCircleShape_setFillColor(shape, brush->color);
+    sfCircleShape_setPosition(shape, pos);
+    sfRenderTexture_drawCircleShape(renderTexture, shape, NULL);
+    sfRenderTexture_display(renderTexture);
+    sfCircleShape_destroy(shape);
+}
