1eeda24aa4514dfea4f4f3a221c158ed6cea64b1
Saltyy <clement.bouret@epitech.eu>
2024-02-22 21:03:20 +0100

    [~] handling button in main functions call

--- src/main.c
+++ src/main.c
@@ -7,6 +7,23 @@
 
 #include "../include/paint.h"
 
+int test_paint(Window *app)
+{
+    draw_window(app);
+    init_drawzone(app);
+    init_button(&bouton);
+    while (sfRenderWindow_isOpen(app->window)){
+        analyse_event(app);
+        display_window(app);
+        display_drawzone(app);
+        updateButtonState(&bouton, app->window);
+        drawButton(app->window, &bouton);
+        sfRenderWindow_display(app->window);
+    }
+    del_window(app);
+    return EXIT_SUCCESS;
+}
+
 int main(int ac, char **av)
 {
     Window *app = malloc(sizeof(Window));
