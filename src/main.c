dec7c3b0006bff9a29f2e36100d11f3e1d1224ad
Saltyy <clement.bouret@epitech.eu>
2024-02-22 17:17:21 +0100

    [~] coding style and header

--- src/main.c
+++ src/main.c
@@ -1,14 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** main.c
+** File description:
+** main function for my_paint
+*/
+
 #include "../include/paint.h"
 
 int main(int ac, char **av)
 {
     Window *app = malloc(sizeof(Window));
 
-    if(ac == 2 && av[1][0] == '-' && av[1][1] == 'h'){
+    if (ac == 2 && av[1][0] == '-' && av[1][1] == 'h'){
         get_help();
         return 0;
     }
-
     test_paint(app);
     free(app);
     return 0;
