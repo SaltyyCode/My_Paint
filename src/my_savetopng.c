e314b0d78b341bf182a5f34d79adb8972b361126
Saltyy <clement.bouret@epitech.eu>
2024-02-22 17:18:06 +0100

    [~] coding style and header

--- src/my_savetopng.c
+++ src/my_savetopng.c
@@ -1,14 +1,21 @@
+/*
+** EPITECH PROJECT, 2024
+** my_savetopng.c
+** File description:
+** function to save drawing in my_paint
+*/
+
 #include "../include/paint.h"
 
-void savedrawtopng(sfRenderTexture* RenderTexture, const char* filePath)
+void savedrawtopng(sfRenderTexture *RenderTexture, const char *filePath)
 {
     const sfTexture* textureDessin;
-    
+    sfImage *image;
+
     if (!RenderTexture || !filePath)
         return;
     textureDessin = sfRenderTexture_getTexture(RenderTexture);
-    sfImage* image = sfTexture_copyToImage(textureDessin);
+    image = sfTexture_copyToImage(textureDessin);
     sfImage_saveToFile(image, filePath);
     sfImage_destroy(image);
-
 }
