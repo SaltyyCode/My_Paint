ef024d7812df5ddb945bca5bd3d1748316d79e38
Saltyy <clement.bouret@epitech.eu>
2024-02-28 17:40:20 +0100

    [~] changed keyp to handle key M to save image in .jpg

--- src/my_handlekey.c
+++ src/my_handlekey.c
@@ -10,7 +10,9 @@
 void keyp(Window *app)
 {
     if (app->event.key.code == sfKeyP)
-                savedrawtopng(app->drawzone, "dessin.png");
+                savedrawtoimage(app->drawzone, "dessin.png");
+    if (app->event.key.code == sfKeyM)
+                savedrawtoimage(app->drawzone, "dessin.jpg");
     if (app->event.key.code == sfKeyB)
                 app->current = BRUSH;
     if (app->event.key.code == sfKeyE)
