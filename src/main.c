115d68e6a805646398d65becd2e120bd0de1ffdc
Saltyy <clement.bouret@epitech.eu>
2024-03-02 22:03:23 +0100

    [~] test_paint & main functions update

--- src/main.c
+++ src/main.c
@@ -7,16 +7,18 @@
 
 #include "../include/paint.h"
 
-int test_paint(Window *app)
+int test_paint(Window *app, color_s *colors)
 {
     draw_window(app);
     init_drawzone(app);
     init_button(&bouton);
     init_button2();
+    init_red(colors);
     while (sfRenderWindow_isOpen(app->window)){
         analyse_event(app);
         display_window(app);
         display_drawzone(app);
+        display_color(colors, app);
         updatebuttonstate(&bouton, app->window);
         drawbutton(app->window, &bouton);
         updatebuttonstate(&bouton2, app->window);
@@ -30,12 +32,14 @@ int test_paint(Window *app)
 int main(int ac, char **av)
 {
     Window *app = malloc(sizeof(Window));
+    color_s *colors = malloc(sizeof(color_s));
 
     if (ac == 2 && av[1][0] == '-' && av[1][1] == 'h'){
         get_help();
         return 0;
     }
-    test_paint(app);
+    test_paint(app, colors);
     free(app);
+    free(colors);
     return 0;
 }
