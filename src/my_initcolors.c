9e463114a82dec7d7f5ed7ee730a7f2754a285af
Clément // Saltyy <clementbouret59@gmail.com>
2024-03-09 00:46:54 +0100

    [~] all sprite colors are called in this function for main

--- src/my_initcolors.c
+++ src/my_initcolors.c
@@ -6,4 +6,7 @@ void init_colors(color_s *colors)
     init_pink(colors);
     init_yellow(colors);
     init_green(colors);
+    init_blue(colors);
+    init_white(colors);
+    init_black(colors);
 }
