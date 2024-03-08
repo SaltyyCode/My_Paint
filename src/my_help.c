cb2fe11615ee4a93f0cd38ba922235e99408efbe
Saltyy <clement.bouret@epitech.eu>
2024-03-08 17:49:50 +0100

    [~] changed -h text

--- src/my_help.c
+++ src/my_help.c
@@ -11,8 +11,8 @@ void get_help(void)
 {
     my_putstr("My_Paint epitech project !\n\n");
     my_putstr("Tools:\n");
-    my_putstr("Pencil : Press P and click or drag to draw\n");
+    my_putstr("Pencil : Press B and click or drag to draw\n");
     my_putstr("Eraser: Press E and click or drag to erase\n");
     my_putstr("Hand: Press H and click or drag to move canva\n");
-    my_putstr("Save the image: Press to save .jpg\n");
+    my_putstr("Save the image: Press P to save .jpg\n");
 }
